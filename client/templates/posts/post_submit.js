ae55456e1e5d1ac73a642bf34e9f0eda00025183
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-14 16:43:12 +0530

    Use a method to submit the post

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -5,8 +5,12 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
-		post._id = Posts.insert(post);
-		//console.log(post._id);
-		Router.go('postPage', post);
+		Meteor.call('postInsert', post, function(error, result) {
+// display the error to the user and abort
+			if (error){
+				return alert(error.reason);
+			}
+			Router.go('postPage', {_id: result._id});
+		});
 	}
 });
