ba9eec9af8d6dfae1cc1b5334a372d9a9d49bf0c
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-17 16:15:41 +0530

    Validate post contents on submission

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,3 +1,7 @@
+Errors = new Mongo.Collection(null);
+throwError = function(message) {
+	Errors.insert({message: message});
+};
 Template.errors.helpers({
 	errors: function() {
 		return Errors.find();
