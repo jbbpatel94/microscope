ec0881789f2f3e880a37e52f7a1795473b465db7
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-17 14:45:44 +0530

    Clear errors after 3 seconds

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,4 +1,11 @@
-Errors = new Mongo.Collection(null);
-throwError = function(message) {
-	Errors.insert({message: message});
+Template.errors.helpers({
+	errors: function() {
+		return Errors.find();
+	}
+});
+Template.error.rendered = function() {
+	var error = this.data;
+	Meteor.setTimeout(function () {
+		Errors.remove(error._id);
+	}, 3000);
 };
