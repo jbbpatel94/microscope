d399a54e65022a62f78b3fafa79c2576a4fd7c37
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-17 10:58:38 +0530

    Demonstrate the order that posts appear using a sleep

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,14 +7,12 @@ Template.postSubmit.events({
 		};
 		Meteor.call('postInsert', post, function(error, result) {
 // display the error to the user and abort
-			if (error){
-				return alert(error.reason);
-			}
+if (error)
+	return alert(error.reason);
 // show this result but route anyway
-			if (result.postExists){
-				alert('This link has already been posted');
-			}
-			Router.go('postPage', {_id: result._id});
-		});
+if (result.postExists)
+	alert('This link has already been posted');
+});
+		Router.go('postsList');
 	}
 });
